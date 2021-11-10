73993f6ce68f9b4d7dff76ef470bef8187b0c539
Grant Duntugan <gduntuga@ucsd.edu>
2021-11-10 12:42:47 -0800

    removed extra components

--- source/scripts/explore-page.js
+++ source/scripts/explore-page.js
@@ -1,12 +1,5 @@
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  // Establish references to HTML elements
-  const body = document.querySelector("body");
-  const navbar = document.createElement("top-navbar");
-  navbar.data = ""; //Initializes shadow root
-  const main = document.querySelector("main");
-
-  //Insert navbar at top of page
-  body.insertBefore(navbar, main);
+  // Functionality to be added later here
 }
