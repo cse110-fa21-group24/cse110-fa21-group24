9a9a106bfb0c2761655ad3bc8e721ce260f101c4
Minh <m6dinh@ucsd.edu>
2021-11-13 01:10:18 -0800

    setup basic spoonacular functions and hid the spoonacular api key

--- source/scripts/spoonacular-interface.js
+++ source/scripts/spoonacular-interface.js
@@ -1,1 +1,49 @@
-let key = "?apikey=b1dfe789b60940b38f1768ccf739ffcf"; //the api key xd
+/* jshint ignore:start */
+require("dotenv").config();
+/* jshint ignore:end */
+
+/**
+ * @classdesc An interface for fetching recipes and recipe information using
+ *            the Spoonacular API at https://spoonacular.com/food-api
+ */
+export class SpoonacularInterface {
+  async getRecipes(paramsObj) {
+    let requestUrl =
+      "https://api.spoonacular.com/recipes/complexSearch?apiKey=" +
+      process.env.SPOONACULAR_API_KEY +
+      "&instructionsRequired=true";
+
+    for (const param in paramsObj) {
+      if (paramsObj.hasOwnProperty(param)) {
+        requestUrl += "&" + param + "=" + paramsObj[param];
+      }
+    }
+
+    return this.makeRequest(requestUrl);
+  }
+
+  async getRandomRecipes() {
+    let requestUrl =
+      "https://api.spoonacular.com/recipes/complexSearch?apiKey=" +
+      process.env.SPOONACULAR_API_KEY +
+      "&instructionsRequired=true&sort=random";
+
+    return this.makeRequest(requestUrl);
+  }
+
+  async getRecipeInfo(id) {
+    let requestUrl =
+      "https://api.spoonacular.com/recipes/" +
+      id +
+      "/information?apiKey=" +
+      process.env.SPOONACULAR_API_KEY;
+
+    return this.makeRequest(requestUrl);
+  }
+
+  async makeRequest(requestUrl) {
+    const response = await fetch(requestUrl);
+    const data = await response.json();
+    return data;
+  }
+}
