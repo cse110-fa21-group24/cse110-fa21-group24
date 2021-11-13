eaff1de3d33cff4d9790ede9f0a9f5a6fd4b228b
Yelllowbelly <>
2021-11-13 12:38:12 -0800

    prettier

--- source/scripts/NavBar.js
+++ source/scripts/NavBar.js
@@ -1,12 +1,12 @@
 class Navbar extends HTMLElement {
-    constructor() {
-      super();
-      this.shadow = this.attachShadow({ mode: "open" });
-    }
-  
-    set data(data) {
-      const styleElem = document.createElement("style");
-      const styles = `
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+  }
+
+  set data(data) {
+    const styleElem = document.createElement("style");
+    const styles = `
           body {
               margin: 0;
             }
@@ -131,70 +131,70 @@ class Navbar extends HTMLElement {
               }
             }
           `;
-      styleElem.innerHTML = styles;
-      this.shadow.appendChild(styleElem);
-  
-      // Body for HTML element
-      const body = document.createElement("body");
-  
-      // Main section of navbar
-      const nav = document.createElement("nav");
-      nav.id = "navbar";
-  
-      // Div which contains the title
-      const leftDiv = document.createElement("div");
-      leftDiv.id = "navbar-section-left";
-      leftDiv.className = "navbar-section";
-  
-      // The article element that is the title
-      const title = document.createElement("a");
-      title.id = "web-app-title";
-      title.href = "/source/navbar.html"; //TODO: Change for navigation
-      title.textContent = "Impasta Rosta";
-  
-      // Div that contains the directories
-      const rightDiv = document.createElement("div");
-      rightDiv.id = "navbar-section-right";
-      rightDiv.className = "navbar-section";
-  
-      // Article and Paragraph elements for Home
-      const homeA = document.createElement("a");
-      homeA.className = "tab";
-      homeA.href = "/source/navbar.html"; //TODO: Change for navigation
-      homeA.textContent = "Home";
-      const homeP = document.createElement("p");
-      homeP.className = "tab-divider";
-      homeP.textContent = "•";
-  
-      // Article and Paragraph elements for Explore
-      const exploreA = document.createElement("a");
-      exploreA.className = "tab";
-      exploreA.href = "/source/navbar.html"; //TODO: Change for navigation
-      exploreA.textContent = "Explore";
-      const exploreP = document.createElement("p");
-      exploreP.className = "tab-divider";
-      exploreP.textContent = "•";
-  
-      // Article and Paragraph elements for Cookbooks
-      const cookbookA = document.createElement("a");
-      cookbookA.className = "tab";
-      cookbookA.href = "/source/navbar.html"; //TODO: Change for navigation
-      cookbookA.textContent = "My Cookbooks";
-  
-      // Combine all according to template
-      body.appendChild(nav);
-      nav.appendChild(leftDiv);
-      leftDiv.appendChild(title);
-      nav.appendChild(rightDiv);
-      rightDiv.appendChild(homeA);
-      rightDiv.appendChild(homeP);
-      rightDiv.appendChild(exploreA);
-      rightDiv.appendChild(exploreP);
-      rightDiv.appendChild(cookbookA);
-  
-      // Add block to shadow element
-      this.shadow.appendChild(body);
-    }
+    styleElem.innerHTML = styles;
+    this.shadow.appendChild(styleElem);
+
+    // Body for HTML element
+    const body = document.createElement("body");
+
+    // Main section of navbar
+    const nav = document.createElement("nav");
+    nav.id = "navbar";
+
+    // Div which contains the title
+    const leftDiv = document.createElement("div");
+    leftDiv.id = "navbar-section-left";
+    leftDiv.className = "navbar-section";
+
+    // The article element that is the title
+    const title = document.createElement("a");
+    title.id = "web-app-title";
+    title.href = "/source/navbar.html"; //TODO: Change for navigation
+    title.textContent = "Impasta Rosta";
+
+    // Div that contains the directories
+    const rightDiv = document.createElement("div");
+    rightDiv.id = "navbar-section-right";
+    rightDiv.className = "navbar-section";
+
+    // Article and Paragraph elements for Home
+    const homeA = document.createElement("a");
+    homeA.className = "tab";
+    homeA.href = "/source/navbar.html"; //TODO: Change for navigation
+    homeA.textContent = "Home";
+    const homeP = document.createElement("p");
+    homeP.className = "tab-divider";
+    homeP.textContent = "•";
+
+    // Article and Paragraph elements for Explore
+    const exploreA = document.createElement("a");
+    exploreA.className = "tab";
+    exploreA.href = "/source/navbar.html"; //TODO: Change for navigation
+    exploreA.textContent = "Explore";
+    const exploreP = document.createElement("p");
+    exploreP.className = "tab-divider";
+    exploreP.textContent = "•";
+
+    // Article and Paragraph elements for Cookbooks
+    const cookbookA = document.createElement("a");
+    cookbookA.className = "tab";
+    cookbookA.href = "/source/navbar.html"; //TODO: Change for navigation
+    cookbookA.textContent = "My Cookbooks";
+
+    // Combine all according to template
+    body.appendChild(nav);
+    nav.appendChild(leftDiv);
+    leftDiv.appendChild(title);
+    nav.appendChild(rightDiv);
+    rightDiv.appendChild(homeA);
+    rightDiv.appendChild(homeP);
+    rightDiv.appendChild(exploreA);
+    rightDiv.appendChild(exploreP);
+    rightDiv.appendChild(cookbookA);
+
+    // Add block to shadow element
+    this.shadow.appendChild(body);
   }
-  
-  customElements.define("top-navbar", Navbar);
+}
+
+customElements.define("top-navbar", Navbar);
