5b3f5763b8d97f254db7da8fdfbd89bac3807879
grantDuntugan <grantdunt@gmail.com>
2021-11-13 15:44:11 -0800

    Wired in explore page and allowed navigation from footer img to explore for testing purposes

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -1,3 +1,5 @@
+import { Router } from "./router.js";
+
 /**
  * Creates a navbar custom element and adds it to the document
  * @function
@@ -19,14 +21,45 @@ function createFooterImg() {
 }
 
 /**
- * Creates the explore page and adds it to the document
+ * Creates the explore page and adds it to the document. This
+ * page starts off hidden.
  * @function
  */
 function createExplorePage() {
   "use strict";
   const explorePage = document.createElement("explore-page");
-  console.log(explorePage);
+  explorePage.classList.toggle("hidden");
   document.querySelector("body").append(explorePage);
+}
+
+/**
+ * Attaches "click" event listeners to the buttons on the navbar
+ * that navigate to the correct page when clicked.
+ */
+function connectNavbarButtons(router) {
+  "use strict";
+  let navbar = document.querySelector("custom-navbar");
+  let shadow = navbar.shadowRoot;
+  let buttons = shadow.querySelectorAll("button.navbar-tab");
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i].textContent === "Home") {
+      buttons[i].addEventListener("click", () => {
+        router.navigate(document.querySelector("footer-img")); //TODO: CHANGE THIS TO HOME ELEMENT WHEN ADDED!!!
+      });
+    }
+
+    if (buttons[i].textContent === "Explore") {
+      buttons[i].addEventListener("click", () => {
+        router.navigate(document.querySelector("explore-page"));
+      });
+    }
+
+    if (buttons[i].textContent === "My Cookbooks") {
+      buttons[i].addEventListener("click", () => {
+        //TODO: router.navigate(document.querySelector("PUT YOUR COOKBOOK UI ELEMENT NAME HERE"));
+      });
+    }
+  }
 }
 
 /**
@@ -38,6 +71,11 @@ async function init() {
   createNavbar();
   createExplorePage();
   createFooterImg();
+
+  //Create the router after all pages have been initialized!!!
+  const router = new Router(document.querySelector("footer-img")); //TODO: CHANGE TO HOME ELEMENT WHEN ADDED IN!!!
+
+  connectNavbarButtons(router);
 }
 
 window.addEventListener("DOMContentLoaded", init);
--- source/scripts/router.js
+++ source/scripts/router.js
@@ -2,4 +2,33 @@
  * @classdesc A class that handles loading the appropriate screens when
  *            navigating through the web app
  */
-export class Router {}
+export class Router {
+  /**
+   * This constructor stores the current page in order to tell when
+   * and what to hide when navigating.
+   *
+   * @param {Element} currentPage
+   */
+  constructor(currentPage) {
+    this.currentPage = currentPage;
+  }
+
+  /**
+   * This function navigates to another page by toggling the "hidden" name
+   * on the element's classList. If the current page is the one that is being
+   * navigated to, then do nothing.
+   *
+   * @param {Element} page
+   */
+  navigate(page) {
+    if (page === this.currentPage) {
+      return;
+    }
+
+    this.currentPage.classList.toggle("hidden");
+    if (page.classList.contains("hidden")) {
+      this.currentPage = page;
+      page.classList.toggle("hidden");
+    }
+  }
+}
