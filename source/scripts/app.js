df786ae143e39baed123c2364f226c126d7a97a1
Minh <m6dinh@ucsd.edu>
2021-11-13 12:26:46 -0800

    wired bottom wave into landscape display

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -1,12 +1,31 @@
+/**
+ * Creates a navbar custom element and adds it to the document
+ * @function
+ */
 function createNavbar() {
   "use strict";
   const navbar = document.createElement("custom-navbar");
   document.querySelector("body").append(navbar);
 }
 
+/**
+ * Creates a wave custom element and adds it to the document
+ * @function
+ */
+function createFooterImg() {
+  "use strict";
+  const footerImg = document.createElement("footer-img");
+  document.querySelector("body").append(footerImg);
+}
+
+/**
+ * Runs initial setup functions when the page first loads
+ * @function
+ */
 async function init() {
   "use strict";
   createNavbar();
+  createFooterImg();
 }
 
 window.addEventListener("DOMContentLoaded", init);
