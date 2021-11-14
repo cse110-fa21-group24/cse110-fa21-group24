967ea47ae99f3f04efd80a78347c4a761e443582
keli214 <77420536+keli214@users.noreply.github.com>
2021-11-14 13:43:51 -0800

    resolve conflict

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -45,6 +45,70 @@ function createCookbookCard() {
   }
 }
 
+function createRecipeForm() {
+  "use strict";
+  const recipeForm = document.createElement("recipe-form");
+  recipeForm.classList.toggle("hidden");
+  document.querySelector("body").append(recipeForm);
+}
+
+/**
+ * Creates a form for creating a new cookbook and adds it to the document
+ * @function
+ */
+function createCreateCookbook() {
+  "use strict";
+  const createCookbook = document.createElement("create-cookbook");
+  // createCookbook.classList.toggle("hidden"); //TODO: UNCOMMENT WHEN HOME WIRED IN
+  document.querySelector("body").append(createCookbook);
+}
+
+/**
+ * Creates the explore page and adds it to the document. This
+ * page starts off hidden.
+ * @function
+ */
+function createExplorePage() {
+  "use strict";
+  const explorePage = document.createElement("explore-page");
+  explorePage.classList.toggle("hidden");
+  document.querySelector("body").append(explorePage);
+}
+
+/**
+ * Attaches "click" event listeners to the buttons on the navbar
+ * that navigate to the correct page when clicked.
+ */
+function connectNavbarButtons() {
+  "use strict";
+
+  //Get references to buttons in shadowRoot
+  let navbar = document.querySelector("custom-navbar");
+  let shadow = navbar.shadowRoot;
+  let buttons = shadow.querySelectorAll("button.navbar-tab");
+
+  //Loop through buttons and establish click listeners on each button
+  for (let i = 0; i < buttons.length; i++) {
+    //Use if statements to check for name for easy style changes in the future
+    if (buttons[i].textContent === "Home") {
+      buttons[i].addEventListener("click", () => {
+        router.navigate("create-cookbook"); //TODO: CHANGE THIS TO HOME ELEMENT WHEN ADDED!!!
+      });
+    }
+
+    if (buttons[i].textContent === "Explore") {
+      buttons[i].addEventListener("click", () => {
+        router.navigate("explore-page"); // Navigate to explore page
+      });
+    }
+
+    if (buttons[i].textContent === "My Cookbooks") {
+      buttons[i].addEventListener("click", () => {
+        //TODO: router.navigate("PUT YOUR COOKBOOK UI ELEMENT NAME HERE");
+      });
+    }
+  }
+}
 /**
  * Runs initial setup functions when the page first loads
  * @function
@@ -53,6 +117,11 @@ async function init() {
   "use strict";
   createNavbar();
   createFooterImg();
+  createRecipeForm();
+  createCreateCookbook();
+  createExplorePage();
+  createFooterImg();
+  connectNavbarButtons();
   createCookbook();
   createCookbookCard();
 }
