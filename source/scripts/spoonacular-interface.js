819251dbadd9e92c494d9e798c9c8422930c3dc4
Minh <m6dinh@ucsd.edu>
2021-11-15 03:03:21 -0800

    added constant to spoonacular interface to enable or disable the interface

--- source/scripts/spoonacular-interface.js
+++ source/scripts/spoonacular-interface.js
@@ -1,4 +1,5 @@
 const SPOONACULAR_API_KEY = "83c84ad2b0e4486f93cfbe9658d21c66";
+const SPOONACULAR_ENABLED = false;
 
 /**
  * @classdesc An interface for fetching recipes and recipe information using
@@ -14,6 +15,10 @@ export class SpoonacularInterface {
    *          title, and image for each retrieved recipe
    */
   async getRecipes(filtersObj) {
+    if (!SPOONACULAR_ENABLED) {
+      return;
+    }
+
     let requestUrl =
       "https://api.spoonacular.com/recipes/complexSearch?apiKey=" +
       SPOONACULAR_API_KEY +
@@ -46,6 +51,10 @@ export class SpoonacularInterface {
    *          title, and image for each retrieved recipe
    */
   async getRandomRecipes(numResults) {
+    if (!SPOONACULAR_ENABLED) {
+      return;
+    }
+
     let requestUrl =
       "https://api.spoonacular.com/recipes/complexSearch?apiKey=" +
       SPOONACULAR_API_KEY +
@@ -73,6 +82,10 @@ export class SpoonacularInterface {
    * @returns An object containing several properties of the recipe
    */
   async getRecipeInfo(id) {
+    if (!SPOONACULAR_ENABLED) {
+      return;
+    }
+
     let requestUrl =
       "https://api.spoonacular.com/recipes/" +
       id +
@@ -110,6 +123,10 @@ export class SpoonacularInterface {
    * @returns The data parsed from the response
    */
   async makeRequest(requestUrl) {
+    if (!SPOONACULAR_ENABLED) {
+      return;
+    }
+
     let response = await fetch(requestUrl);
     let data = await response.json();
     return data;
