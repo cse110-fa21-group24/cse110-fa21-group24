ff67617e4a3c758d956c0ab7aa0f9e43fe3fee18
Minh <m6dinh@ucsd.edu>
2021-11-23 22:02:44 -0800

    fixed linting issue

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -585,8 +585,7 @@ function populateRecipePage(recipeObj, fromSpoonacular) {
   }
 
   shadow.getElementById("recipe-image").src = recipeObj.image;
-  shadow.getElementById("recipe-description").innerHTML =
-    recipeObj.description;
+  shadow.getElementById("recipe-description").innerHTML = recipeObj.description;
 
   let ingredientsLeft = shadow.getElementById(
     "recipe-ingredients-section-left"
